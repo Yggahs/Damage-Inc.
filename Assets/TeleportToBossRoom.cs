7d2666b217abb4afcee8b4e6deb4a2fcabab1705
Yggahs <alessio.calore96@gmail.com>
2019-03-25 20:45:34 +0000

    Added teleport to boss room, player spritesheets are in

---
+++ Assets/TeleportToBossRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToBossRoom : MonoBehaviour {
+    public GameObject PlayerRef;
+    public GameObject BossRoomEntrace;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerRef.transform.position = BossRoomEntrace.transform.position;
+        }
+    }
+}
