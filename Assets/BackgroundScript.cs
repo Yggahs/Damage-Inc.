11ec868fc854880fa83cdeb6aa3d987e1b52995b
Yggahs <alessio.calore96@gmail.com>
2019-04-01 16:21:59 +0100

    Fixed tile gap and finishedbackground transitions

--- Assets/BackgroundScript.cs
+++ Assets/BackgroundScript.cs
@@ -7,11 +7,19 @@ public class BackgroundScript : MonoBehaviour {
     public GameObject PlayerRef;
     public float bgSpeed;
     public Renderer bgRend = null;
-	// Use this for initialization
+    private Camera cam;
 
-	
-	// Update is called once per frame
-	void Update () {
-        bgRend.material.mainTextureOffset += new Vector2(bgSpeed*PlayerRef.GetComponent<CharacterController2D>().backgroundSpeed*Time.deltaTime,0f);
-	}
+    public Material forest;
+    public Material lab;
+    public Material offices;
+    // Update is called once per frame
+    private void Start()
+    {
+        cam = Camera.main;
+        Debug.Log(cam.name);
+    }
+    void Update () {
+        transform.localScale = new Vector3(cam.orthographicSize * 2.0f * Screen.width / Screen.height, cam.orthographicSize * 2.0f, 0.1f);
+        bgRend.material.mainTextureOffset += new Vector2(bgSpeed*PlayerRef.GetComponent<CharacterController2D>().backgroundSpeed*Time.deltaTime*0.2f,0f);
+    }
 }
---
+++ Assets/SwitchZone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchZone : MonoBehaviour {
+    GameObject PlayerRef;
+    Material currentMat;
+    Material forestMat;
+    Material labMat;
+    Material officeMat;
+    Material targetMat;
+    private void Start()
+    {
+        PlayerRef = GameObject.FindGameObjectWithTag("GameController");
+        
+        labMat = PlayerRef.transform.GetChild(6).GetComponent<BackgroundScript>().lab;
+        forestMat = PlayerRef.transform.GetChild(6).GetComponent<BackgroundScript>().forest;
+        officeMat = PlayerRef.transform.GetChild(6).GetComponent<BackgroundScript>().offices;
+
+        targetMat = forestMat;
+
+    }
+    public void SwitchToForest()
+    {
+        if (targetMat != forestMat)
+        {
+            targetMat = forestMat;
+            PlayerRef.transform.GetChild(6).GetComponent<BackgroundScript>().bgRend.material = targetMat;
+        }
+    }
+    public void SwitchToLab()
+    {
+        if (targetMat != labMat)
+        {
+            targetMat = labMat;
+            PlayerRef.transform.GetChild(6).GetComponent<BackgroundScript>().bgRend.material = targetMat;
+        }
+    }
+    public void SwitchToOffices()
+    {
+        if (targetMat != labMat)
+        {
+            targetMat = officeMat;
+            PlayerRef.transform.GetChild(6).GetComponent<BackgroundScript>().bgRend.material = targetMat;
+        }
+    }
+}
