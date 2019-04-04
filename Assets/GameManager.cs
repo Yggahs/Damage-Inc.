0a992af450e62288da29045eb3476e6fca147fa0
Dario Tabatabie <druser@hotmail.it>
2019-04-04 14:04:44 +0100

    Implementing all the graphical assets that we've just got

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject BombsUI, CollectUI;
-    public TMP_Text Title, Description, InputButton;
+    public Text Title, Description, InputButton;
 
     // Use this for initialization
     void Start ()
     {
-        Title = GameObject.Find("Title").GetComponent<TMP_Text>();
-        Description = GameObject.Find("Description").GetComponent<TMP_Text>();
-        InputButton = GameObject.Find("InputButton").GetComponent<TMP_Text>();
+        Title = GameObject.Find("Title").GetComponent<Text>();
+        Description = GameObject.Find("Description").GetComponent<Text>();
+        InputButton = GameObject.Find("InputButton").GetComponent<Text>();
         CollectUI = GameObject.Find("CollectUI");
         BombsUI = GameObject.Find("BombInfos");
 
